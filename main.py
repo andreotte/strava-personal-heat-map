ccc521f2c7a7030c3b35802fa45d9ea98a9d464d
Andre Otte <aotte@bizstream.com>
2020-05-28 11:49:22 -0400

    get rid of keys and cleanup

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
 #!python3
+import getpass
 import time
 import json
+import os
 import requests
-from requests.auth import HTTPBasicAuth, HTTPDigestAuth     
+import webbrowser
+from requests.auth import HTTPBasicAuth, HTTPDigestAuth  
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.chrome.options import Options
 
-urlBase = "https://www.strava.com/api/v3/"
-headers = {'Authorization': 'Bearer 2c42930e9ab59b5452b510f0a68a8587e8347b3e'}
+apiBase = "https://www.strava.com/api/v3/"
+# headers = {'Authorization': 'Bearer abc'} #the bearer comes from authorization post
 
 #returns parsed json as a dict
-#params are ia dict or params
-def request(request, params = None):
-        url = urlBase + request
+#params are a dict of params
+def request(urlBase, request, params = None):
+        url = apiBase + request
         response = requests.get(url, headers=headers, params=params)   # modify request headers 
         print(request, " - ResponseCode: ", response.status_code)
         return response.json()
 
+def parseAuthBearer(url):
+        url2 = url.split('code=')
+        bearer = url2[1].split('&scope')[0]
+
+def authorizeStravaApp():
+        clientID = #add strava ClientID here
+        oauthUrl = (f"https://www.strava.com/oauth/authorize?client_id={clientID}&"
+                        "response_type=code&"
+                        "redirect_uri=http://localhost/exchange_token&"
+                        "approval_prompt=force&"
+                        "scope=read,activity:read_all")
+
+        driver = webdriver.Firefox(executable_path=r'C:\\Program Files\\Geckodriver\\geckodriver.exe') #might need to change this path to geckodriver
+        driver.get(oauthUrl)
+        
+        someVariable = getpass.getpass("Press Enter after You are done logging in") 
+        print("You are logged in. Continuing script *beep bop boop*")
+        
+        driver.find_element_by_id("authorize").click()
+        url2 = driver.current_url.split('code=')
+        return url2[1].split('&scope')[0]
+
 def getAthlete():
-        athlete = request("athlete")
+        athlete = request(apiBase, "athlete")
         print(athlete["id"])        
 
 def getActivityRoutes(page = 1, activityRoutes = []):
@@ -25,10 +53,10 @@ def getActivityRoutes(page = 1, activityRoutes = []):
 
         params = {'after': timeStart, 'before': timeEnd, 'page': page}
 
-        activityList = request("athlete/activities", params)
+        activityList = request(apiBase, "athlete/activities", params)
 
         # if page < 2: #for testing
-        if len(activityList) > 0: #this is what we need. but we are commenting it out to keep it short.
+        if len(activityList) > 0: #comment this line out fortesting to keep it short.
                 for activity in activityList:
                         if activity["map"]["summary_polyline"]:
                                 activityRoutes.append(activity["map"]["summary_polyline"])
@@ -38,14 +66,10 @@ def getActivityRoutes(page = 1, activityRoutes = []):
                 print(len(activityRoutes), "routes found")
                 return activityRoutes
 
-# def writeAvtivityRouteToFile(activityRoute, routeNumber):
-#         file = open("Activities.txt","a")
-#         file.write(f'let route{routeNumber} = "{activityRoute}"')
-#         file.write('\n')
-#         file.close() 
-
-
 def main():
+        bearer = authorizeStravaApp()
+        headers = {'Authorization': f'Bearer {bearer}'} #the bearer comes from authorization post
+        # print(headers)
         activityRoutes = getActivityRoutes()
         file = open("Activities.txt","a")
         file.write(f'let routes = [')
@@ -56,9 +80,6 @@ def main():
                 i += 1
         file.write('];')
         file.close() 
-
-        
-        # getAthlete()
 
 if __name__ == '__main__':
     main()
