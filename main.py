85850870425fff2a3abd42bdf7f8a443d4a2335c
Andre Otte <aotte@bizstream.com>
2020-05-29 12:20:10 -0400

    everything done automagically

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import getpass
 import time
 import json
 import os
+import re
 import requests
 import webbrowser
 from requests.auth import HTTPBasicAuth, HTTPDigestAuth  
@@ -11,41 +12,51 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
 apiBase = "https://www.strava.com/api/v3/"
-# headers = {'Authorization': 'Bearer abc'} #the bearer comes from authorization post
+clientID = #Custom Input - Strava Client ID
+client_secret = #Custom Input - Strava Client Secret
+headers = '' 
 
-#returns parsed json as a dict
-#params are a dict of params
+driver = webdriver.Firefox(executable_path=r'C:\\Program Files\\Geckodriver\\geckodriver.exe') #Custom Input - local path to Selenium geckodriver
+
+#returns parsed json as a dictionary
+#params are a dictionary of params
 def request(urlBase, request, params = None):
         url = apiBase + request
-        response = requests.get(url, headers=headers, params=params)   # modify request headers 
-        print(request, " - ResponseCode: ", response.status_code)
+        response = requests.get(url, headers=headers, params=params)
+        # print("request endpoint: ", request, " - ResponseCode: ", response.status_code)
         return response.json()
 
-def parseAuthBearer(url):
-        url2 = url.split('code=')
-        bearer = url2[1].split('&scope')[0]
-
 def authorizeStravaApp():
-        clientID = #add strava ClientID here
         oauthUrl = (f"https://www.strava.com/oauth/authorize?client_id={clientID}&"
                         "response_type=code&"
                         "redirect_uri=http://localhost/exchange_token&"
                         "approval_prompt=force&"
                         "scope=read,activity:read_all")
 
-        driver = webdriver.Firefox(executable_path=r'C:\\Program Files\\Geckodriver\\geckodriver.exe') #might need to change this path to geckodriver
         driver.get(oauthUrl)
+        print("What's happening?")
+        print("Use Selenium to make a GET request to www.strava.com/oauth/authorize")
+        print("\n")
         
-        someVariable = getpass.getpass("Press Enter after You are done logging in") 
+        print("Hitting 'enter' POSTs to www.strava.com/oauth/token with 'client_id', 'client_secret', and 'code' parameters that was returned from the previous GET request")
+        print("\n")
+        someVariable = getpass.getpass("Press 'enter' after you are done logging in.") 
+        
         print("You are logged in. Continuing script *beep bop boop*")
-        
+        print("\n")
         driver.find_element_by_id("authorize").click()
         url2 = driver.current_url.split('code=')
-        return url2[1].split('&scope')[0]
+        code = url2[1].split('&scope')[0]
 
-def getAthlete():
-        athlete = request(apiBase, "athlete")
-        print(athlete["id"])        
+        data = {f'client_id':clientID,    
+                'client_secret':client_secret, 
+                'code':code,
+                'grant_type':'authorization_code'}
+
+        access_token = requests.post(url = "https://www.strava.com/oauth/token", data = data).json()['access_token']
+        global headers 
+        headers = {'Authorization': f'Bearer {access_token}'}
+        driver.get("file:///C:/Personal/Strava/StravaActivities/loader.html")
 
 def getActivityRoutes(page = 1, activityRoutes = []):
         timeStart = 0 #1970
@@ -54,33 +65,39 @@ def getActivityRoutes(page = 1, activityRoutes = []):
         params = {'after': timeStart, 'before': timeEnd, 'page': page}
 
         activityList = request(apiBase, "athlete/activities", params)
-
-        # if page < 2: #for testing
-        if len(activityList) > 0: #comment this line out fortesting to keep it short.
+        # print(activityList)
+        # if page < 2: # uncomment this for testing to keep the request short
+        if len(activityList) > 0: #comment this line out for testing to keep it short.
                 for activity in activityList:
                         if activity["map"]["summary_polyline"]:
                                 activityRoutes.append(activity["map"]["summary_polyline"])
                 
+                print(len(activityRoutes), "routes found")
                 return getActivityRoutes(page + 1, activityRoutes)
         else:
-                print(len(activityRoutes), "routes found")
                 return activityRoutes
-
+        # else: # uncomment this for testing to keep the request short
+        #      return activityRoutes #for testing  
 def main():
-        bearer = authorizeStravaApp()
-        headers = {'Authorization': f'Bearer {bearer}'} #the bearer comes from authorization post
-        # print(headers)
+        authorizeStravaApp() 
+
+        print("What is happening?")
+        print("We are making a bunch of requests to https://www.strava.com/api/v3/athlete/activities, which returns routes with encoded polylines in batches of ~30. The route polyline strings are being written to a JS file to create an array. This array will be used to populate a map using Mapbox and leaflet.js. When all the polylines have been parsed, Selenium will open map.html.") 
+        print("\n")
         activityRoutes = getActivityRoutes()
-        file = open("Activities.txt","a")
+
+        file = open("activities.js","w")
         file.write(f'let routes = [')
         file.write('\n')
         i = 1
         for activityRoute in activityRoutes:
+                activityRoute = activityRoute.replace("\\", "\\\\")
                 file.write(f'"{activityRoute}",\n')
                 i += 1
         file.write('];')
-        file.close() 
+        file.close()
 
+        driver.get("") #Custom Input - local path to StravaActivities/map.html
 if __name__ == '__main__':
     main()
 
--- myMap.js
+++ myMap.js
@@ -1,20 +1,16 @@
-let routes = [
-    //paste encoded routes as comma separates strings from Activities.txt
-];
-    
 window.onload = function() {
     initMap();
 };
 
 function initMap(){
-    var mymap = L.map('mapid').setView([42.99, -85.66], 18); //Set lat and lng of the map center and zoom level here
+    var mymap = L.map('mapid').setView([42.99, -85.66], 15); //Custom Input - Map center point and zoom level
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox/streets-v11',
         tileSize: 512,
         zoomOffset: -1,
-        accessToken: //mapbox access token here
+        accessToken: //Custom Input - Mapbox access token 
     }).addTo(mymap);
     
     addRoutesToMap(routes, mymap);
@@ -22,14 +18,11 @@ function initMap(){
 
 function addRoutesToMap(routes, mymap){
     for(let i = 0; i < routes.length; i ++){
-        var latLongs =  "";
-        // var polyline = L.Polyline.fromEncoded(routes[i]).addTo(mymap);
-        
+        var latLongs =  ""; 
         var polylineDecoded = L.Polyline.fromEncoded(routes[i])
-        // var polylineDecoded = polyline.decode(routes[i]);
         var modulus = Math.ceil(polylineDecoded._latlngs.length / 100);
-
         var snappedCoords = polylineDecoded._latlngs;
+
         for(var j = 0; j < polylineDecoded._latlngs.length; j ++)
         {
             if(j % modulus == 0){
